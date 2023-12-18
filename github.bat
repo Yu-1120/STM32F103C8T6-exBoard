f923c07eb01303e76a9788068a6f9a4af97ac989
Yu-1120 <1186893249@qq.com>
2023-12-18 13:57:59 +0800

    update+2023/12/18 周一+13:57:59.70

--- github.bat
+++ github.bat
@@ -1,8 +1,9 @@
-git add .  
+git add .
 
-git commit -m "update" 
+echo  %date%+%time%
+
+git commit -m "update+%date%+%time%"
 
 git push -f --set-upstream origin master:master
-
 
 echo File upload successful
