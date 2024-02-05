82169abb1ead588e7b372f296695ae99a553675b
Yu-1120 <1186893249@qq.com>
2024-02-06 00:12:11 +0800

    update+2024/02/06 周二+ 0:12:11.37

--- 2-Software/23-12-17-无源蜂鸣器/Hardware/Beep.h
+++ 2-Software/23-12-17-无源蜂鸣器/Hardware/Beep.h
@@ -1,13 +1,17 @@
 #ifndef __BEEP_H
 #define __BEEP_H
 
-
+#include "Delay.h"
 #define  BEEP_PIN  GPIO_Pin_11
 #define  BEEP_GPIO  GPIOB
 
 void BEEP_Init(void);
 void BEEP_ON(void);
 void BEEP_OFF(void);
-
+void Sound(u16 frq);
+void Sound2(u16 time);
+void play_music(void);
+void play_successful(void);
+void play_failed(void);
 
 #endif
--- 2-Software/23-12-17-无源蜂鸣器/User/main.c
+++ 2-Software/23-12-17-无源蜂鸣器/User/main.c
@@ -10,29 +10,10 @@ int main(void)
 	LED_Init();		//LED初始化
 	Key_Init();		//按键初始化
 	LED1_OFF();			//LED1翻转
-    //BEEP_Init();
+    BEEP_Init();
 	while (1)
 	{
-        
-        
-#if 0
-        BEEP_OFF();  //不响
-        
-#else
-   	    KeyNum = Key_GetNum();		//获取按键键码
-		
-		if (KeyNum == 1)			//按键1按下
-		{
-			LED1_ON();
-		}
-		
-		if (KeyNum == 2)			//按键2按下
-		{
-			LED1_OFF();
-
-		}         
-#endif
-        
-	
+			
+        play_music();
 	}
 }
