19ef2586fc484c43cb0c80b497d4b07625f49ef9
Yu-1120 <1186893249@qq.com>
2024-02-17 00:52:55 +0800

    update+2024/02/17 周六+ 0:52:55.70

---
+++ 2-Software/Stand/24-2-14-定时器定时中断/Hardware/LED.h
@@ -0,0 +1,14 @@
+#ifndef __LED_H
+#define __LED_H
+
+
+#define  LED_PIN  GPIO_Pin_13
+#define  LED_GPIO  GPIOC
+
+void LED_Init(void);
+void LED1_ON(void);
+void LED1_OFF(void);
+void LED1_Turn(void);
+
+
+#endif
